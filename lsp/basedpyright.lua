0016358e72da1021873343d540adf145964086b7
tomystack <aaaris011@gmail.com>
2026-04-28 22:18:16 +0800

    feat: add lsp file

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,3 +1,16 @@
+---@type vim.lsp.Config
 return {
-
+  ---@type lspconfig.settings.basedpyright
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = 'openFilesOnly',
+        -- https://docs.basedpyright.com/latest/configuration/language-server-settings/
+        -- Explicitly setting `basedpyright.analysis.useLibraryCodeForTypes` is **discouraged** by the official docs.
+        -- Because it will override per-project configurations like `pyproject.toml`.
+        -- If left unset, its default value is `true`, and it can be correctly overridden by project config files.
+      },
+    },
+  }
 }
--- lsp/biome.lua
+++ lsp/biome.lua
@@ -1,1 +1,18 @@
-return {}
+---@type vim.lsp.Config
+return {
+  fimetypes = {
+    'astro',
+    'css',
+    'graphql',
+    'html',
+    'javascript',
+    'javascriptreact',
+    'json',
+    'jsonl',
+    'jsonc',
+    'svelte',
+    'typescript',
+    'typescriptreact',
+    'vue',
+  },
+}
